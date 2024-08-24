f5bbfc37510b1ca472a94e46b7af7fcd97dbe0ee
Moh. Rafli Dwi Saputra <133976925+Rap404@users.noreply.github.com>
2024-08-24 11:03:36 +0700

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
-  const name = "john";
-  const x = 10;
-  const y = 20;
-  const names = ["Brad", "Mary", "Joe", "Sara"];
-  const loggedIn = false;
-  const styles = {
-    color: "red",
-    fontSize: "55px",
-  };
-
   return (
-    <div>
-      <div className="text-5xl">App</div>
-      <p>hello {name}</p>
-      <p>
-        {x} + {y} = {x + y}
-      </p>
-      <ul>
-        {names.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-      {/* {loggedIn ? <h1>Hello member</h1> : ""} */}
-      {loggedIn && <h1>Hello member</h1>}
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
   );
 };
 
